1ee792ef7f69c185113dd3890291f761c492230a
mahaseth <yogeshmahaseth07@gmail.com>
2025-11-09 21:32:19 +0545

    Added api-gateway service

---
+++ backend/API-Gateway/src/test/java/com/expensetracker/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensetracker.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
